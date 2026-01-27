1e47988bb9f1d11e6555f2ca770f8be5dce7fc56
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2026-01-27 09:23:39 +0530

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio_app/mobile/landing_page_mobile.dart';
+import 'package:my_portfolio_app/routes/routes.dart';
 import 'package:my_portfolio_app/web/landing_page_web.dart';
 
 void main() {
@@ -15,15 +16,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Portfolio App',
-      home: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth > 1200) {
-            return const LandingPageWeb();
-          } else {
-            return const LandingPageMobile();
-          }
-        },
-      ),
+      // home: LayoutBuilder(
+      //   builder: (context, constraints) {
+      //     if (constraints.maxWidth > 1200) {
+      //       return const LandingPageWeb();
+      //     } else {
+      //       return const LandingPageMobile();
+      //     }
+      //   },
+      // ),
+      onGenerateRoute: (settings) => Routes.generateRoute(settings),
+      initialRoute: '/',
     );
   }
 }
---
+++ lib/routes/routes.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio_app/mobile/landing_page_mobile.dart';
+import 'package:my_portfolio_app/web/landing_page_web.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (context) => LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth > 950) {
+                return const LandingPageWeb();
+              } else {
+                return const LandingPageMobile();
+              }
+            },
+          ),
+          settings: settings,
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (context) => Container(
+            color: Colors.black,
+            child: Center(
+              child: Text(
+                'No route defined for ${settings.name}',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        );
+    }
+  }
+}
